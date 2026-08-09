1bc5a1108e318b04b6062cbe9b5e4bb1ed0b5d8a
Rahma Moktar <rahma.moktar122@gmail.com>
2026-08-09 14:38:34 +0300

    Create TopBar component

---
+++ frontend/src/components/TopBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TopBar() {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <span>+252 63 456 7890</span>
+        <span>info@sabrabanquet.com</span>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
