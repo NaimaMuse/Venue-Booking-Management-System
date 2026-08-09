4bfd60cf25b8ff124f284689d6217699b782c3dc
Rahma Moktar <rahma.moktar122@gmail.com>
2026-08-09 14:41:04 +0300

    Add TopBar social links

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
+
+const socialLinks = ['Fb', 'Ig', 'In'];
+<div className="top-bar-left">
+
+    <div className="social-links">
+  {socialLinks.map((label) => (
+    <a href="/" key={label}>
+      {label}
+    </a>
+  ))}
+</div>
+  <span>+252 63 456 7890</span>
+  <span>info@sabrabanquet.com</span>
+</div>
+
 function TopBar() {
   return (
     <div className="top-bar">
