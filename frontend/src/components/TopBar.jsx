2a0ba7b6ff3d8fcb8d6043ea88815d85953b93b5
Rahma Moktar <rahma.moktar122@gmail.com>
2026-08-09 14:47:38 +0300

    change

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,41 +1,35 @@
 import React from 'react';
 
-
 const socialLinks = ['Fb', 'Ig', 'In'];
-<div className="top-bar-left">
-
-    <div className="social-links">
-  {socialLinks.map((label) => (
-    <a href="/" key={label}>
-      {label}
-    </a>
-  ))}
-</div>
-  <span>+252 63 456 7890</span>
-  <span>info@sabrabanquet.com</span>
-</div>
 
 function TopBar() {
   return (
     <div className="top-bar">
       <div className="top-bar-left">
-        <span>+252 63 456 7890</span>
-        <span>info@sabrabanquet.com</span>
+        <div className="social-icons">
+          {socialLinks.map((label) => (
+            <a key={label} href="/" className="social-icon" aria-label={label}>
+              {label}
+            </a>
+          ))}
+        </div>
+        <span className="top-contact">+252 63 456 7890</span>
+        <span className="top-contact">info@sabrabanquet.com</span>
       </div>
 
       <div className="top-bar-right">
-  <select className="location-select" defaultValue="Hargeisa">
-    <option value="Hargeisa">Hargeisa</option>
-    <option value="Berbera">Berbera</option>
-    <option value="Borama">Borama</option>
-  </select>
+        <select className="location-select" defaultValue="Hargeisa">
+          <option value="Hargeisa">Hargeisa</option>
+          <option value="Berbera">Berbera</option>
+          <option value="Borama">Borama</option>
+        </select>
 
-  <div className="auth-links">
-    <a href="/">Log In</a>
-    <span>|</span>
-    <a href="/">Sign Up</a>
-  </div>
-</div>
+        <div className="auth-links">
+          <a href="/">Log In</a>
+          <span>|</span>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
     </div>
   );
 }
