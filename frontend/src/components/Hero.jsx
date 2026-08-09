75e1179d1fd60b7d24c25c109f6a03f18a8e9d6e
Rahma Moktar <rahma.moktar122@gmail.com>
2026-08-09 19:47:30 +0300

    Create Hero component

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section className="hero-section">
+      <div className="hero-inner">
+        <div className="hero-content">
+          <h1 className="hero-title">Hargeisa Hall Finder</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
