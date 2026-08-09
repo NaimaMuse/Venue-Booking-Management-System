6747ac7e2ddc5a789a58459d977785b3e15dbf1a
Rahma Moktar <rahma.moktar122@gmail.com>
2026-08-09 11:20:22 +0300

    Add Home page imports

---
+++ frontend/src/pages/public website/Home.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import AboutUs from '../components/AboutUs';
+import ContactSection from '../components/ContactSection';
+import FeaturedVenues from '../components/FeaturedVenues';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+
