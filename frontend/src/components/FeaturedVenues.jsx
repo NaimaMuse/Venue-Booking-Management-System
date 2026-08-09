43fd79246d76e3626b7c4a0a890610f5f96c4d2a
Rahma Moktar <rahma.moktar122@gmail.com>
2026-08-09 19:35:37 +0300

    Create FeaturedVenues component

---
+++ frontend/src/components/FeaturedVenues.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FeaturedVenues() {
+  return (
+    <section className="featured-venues-section">
+      <span className="section-label">Featured Halls</span>
+      <h2>Popular Halls in Hargeisa</h2>
+    </section>
+  );
+}
+
+export default FeaturedVenues;
